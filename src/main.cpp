633c0b8a2f96497f833bfbd6c3c19e58f4582092
Michael Skiles <mjrskiles@gmail.com>
2021-10-12 21:04:16 -0500

    usb midi test worked

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "midi_stream.h"
 #include "midi_stream_parser.h"
 #include "midi_file_dumps.h"
-#include <MIDI.h>
+#include <USB-MIDI.h>
 
 const char *file_path = "midi-files/zelda-overworld.mid";
 uint32_t sz = ZELDA_FILE_BYTES_SIZE;
@@ -10,13 +10,15 @@ MidiStream midiStream = MidiStream(zelda_theme, sz);
 MidiParser midiParser = MidiParser(midiStream);
 elapsedMillis ms;
 bool flip = false;
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial1,  MIDI);
+const int channel = 1;
+
+//MIDI comm
+USBMIDI_CREATE_DEFAULT_INSTANCE();
 void setup() {
-    Serial.begin(115200);
+    pinMode(40, INPUT_PULLUP);
     delay(1000);
     Serial.printf("Buffer size: %d\n", midiStream.getSize());
-    MIDI.begin();
-
+    usbMIDI.begin();
     if (midiParser.isAvailable()) {
         Serial.println("Midi header read. Parser available");
         midiParser.advanceBy(8);
@@ -25,16 +27,20 @@ void setup() {
         TrackEvent trkE1 = midiParser.readEvent();
         Serial.printf("Track 1 status: %x\n");
     }
+
 }
 
 void loop() {
 // write your code
-    if (ms > 1000) {
+if (ms > 500) {
         ms = 0;
         if (flip) {
-            MIDI.sendNoteOff(44, 127, 1);
+            usbMIDI.sendNoteOff(44, 127, 1);
+            flip = false;
         } else {
-            MIDI.sendNoteOn(44, 127, 1);
+            usbMIDI.sendNoteOn(44, 127, 1);
+            Serial.println("Sending note on");
+            flip = true;
         }
 
     }
