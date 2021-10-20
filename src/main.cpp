3667f20b154f6527f3ac82194517b1d66e41f9ae
Michael Skiles <mjrskiles@gmail.com>
2021-10-19 19:52:46 -0500

    added usbhost

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,15 @@ const int channel = 1;
 uint32_t trackLen = 0;
 
 //MIDI comm
+USBHost usbHost;
+MIDIDevice teensyMidi(usbHost);
 USBMIDI_CREATE_DEFAULT_INSTANCE();
 void setup() {
     pinMode(40, INPUT_PULLUP);
     delay(3000);
     Serial.printf("Buffer size: %d\n", midiStream.getSize());
     usbMIDI.begin();
+    usbHost.begin();
     if (midiParser.isAvailable()) {
         Serial.println("Midi header read. Parser available");
         midiParser.advanceBy(62);
@@ -45,6 +48,9 @@ void loop() {
         timerTarget = nextEvent.deltaT * 1000; //delta T is in microseconds
         eventTimer = 0;
         waiting = true;
+
+
+        teensyMidi.sendNoteOn(40, 127, 1);
     }
 
     if (waiting && eventTimer >= timerTarget) {
@@ -53,5 +59,7 @@ void loop() {
         Serial.printf("Status %x | data 1 %x | data 2 %x | chan %d\n", nextEvent.event.status, nextEvent.event.data[0],
                       nextEvent.event.data[1], nextEvent.event.channel);
         waiting = false;
+
+        teensyMidi.sendNoteOff(40, 127, 1);
     }
 }
